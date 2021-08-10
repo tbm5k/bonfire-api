8916760f163429fb233c3cc8d2eb694adfba5a3c
Teddy <tedburg5@gmail.com>
2021-08-10 12:51:04 +0300

    initial

---
+++ src/main/java/com/bonfireadventures/BonfireAdventuresApplication.java
@@ -0,0 +1,13 @@
+package com.bonfireadventures;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonfireAdventuresApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonfireAdventuresApplication.class, args);
+	}
+
+}
