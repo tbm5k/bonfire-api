7b5984c69034853dff063402d6eceeb7432101fb
Teddy <tedburg5@gmail.com>
2021-11-30 13:01:03 +0300

    auth wih user details service

--- src/main/java/com/bonfireadventures/security/SecurityConfig.java
+++ src/main/java/com/bonfireadventures/security/SecurityConfig.java
@@ -1,48 +1,36 @@
 package com.bonfireadventures.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
-    private PasswordEncoder passwordEncoder;
+    private MyUserDetailsService myUserDetailsService;
+
+    //Authentication
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(myUserDetailsService);
+    }
 
     //Authorization
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers(HttpMethod.GET, "/continent/**")
-                .permitAll()
+                .antMatchers(HttpMethod.GET, "/continent/**").permitAll()
+                .antMatchers(HttpMethod.POST, "/continent/**", "/continent").hasRole("ADMIN")
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
-    }
-
-    //Inmemory user
-    @Override
-    @Bean
-    protected UserDetailsService userDetailsService() {
-
-        UserDetails admin = User.builder()
-                .username("bonfireadmin")
-                .password(passwordEncoder.encode("password"))
-                .roles("ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin);
     }
 }
