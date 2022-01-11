4aa414ec60cd1674b15c931a41abb1fdb330cb1a
tbm5k <tedburg5@gmail.com>
2022-01-11 22:54:59 +0300

    updated the hotel field in the price package to map a package to the a hotel

--- src/main/java/com/bonfireadventures/controllers/PricePackageController.java
+++ src/main/java/com/bonfireadventures/controllers/PricePackageController.java
@@ -1,6 +1,7 @@
 package com.bonfireadventures.controllers;
 
 import com.bonfireadventures.entities.PricePackage;
+import com.bonfireadventures.services.HotelService;
 import com.bonfireadventures.services.PricePackageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,11 +14,13 @@ public class PricePackageController {
 
     @Autowired
     private PricePackageService pricePackageService;
+    @Autowired
+    private HotelService hotelService;
 
-    @RequestMapping(method = RequestMethod.POST,
-            value = "/continent/{continentId}/country/{countryId}/city/{cityId}/hotel/{hotelId}")
+    @RequestMapping(method = RequestMethod.POST, value = "/continent/{continentId}/country/{countryId}/city/{cityId}/hotel/{hotelId}")
     public ResponseEntity<PricePackage> addPricePackage(@RequestBody PricePackage pricePackage, @PathVariable int continentId, @PathVariable int countryId, @PathVariable int cityId, @PathVariable int hotelId){
         URI uri = URI.create("/continent/" + continentId +"/country/" + countryId + "/city/" + cityId + "/hotel/" + hotelId);
+        pricePackage.setHotelPackage(hotelService.getHotel(continentId, countryId, cityId, hotelId));
         PricePackage response = pricePackageService.addPricePackage(pricePackage);
         return ResponseEntity.created(uri).body(response);
     }
