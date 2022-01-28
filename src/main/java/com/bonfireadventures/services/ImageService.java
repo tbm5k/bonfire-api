d0f6fd96750acb62500494ae9001c528ee10e279
tbm5k <tedburg5@gmail.com>
2022-01-28 16:28:17 +0300

    method to create a bucket

--- src/main/java/com/bonfireadventures/services/ImageService.java
+++ src/main/java/com/bonfireadventures/services/ImageService.java
@@ -2,7 +2,12 @@ package com.bonfireadventures.services;
 
 import com.bonfireadventures.entities.Image;
 import com.bonfireadventures.repositories.ImageRepo;
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -24,5 +29,20 @@ public class ImageService {
             return imageRepo.findById(imageId).get();
         }
         return null;
+    }
+
+    @Bean
+    public void cloudStorage() throws Exception{
+        //instantiate the client
+        Storage storage = StorageOptions.getDefaultInstance().getService();
+
+        //new bucket name
+        String bucketName = "bonfire-adventures-bucket";
+
+        //create bucket
+        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+
+        System.out.println(bucket.getName());
+
     }
 }
