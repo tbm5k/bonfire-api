df3fec155a9dfd1a0e16a66e7678e1f0c13032c0
tbm5k <tedburg5@gmail.com>
2022-01-28 22:29:17 +0300

    bucket creation

--- src/main/java/com/bonfireadventures/services/ImageService.java
+++ src/main/java/com/bonfireadventures/services/ImageService.java
@@ -2,13 +2,20 @@ package com.bonfireadventures.services;
 
 import com.bonfireadventures.entities.Image;
 import com.bonfireadventures.repositories.ImageRepo;
+import com.google.auth.Credentials;
+import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.storage.Bucket;
 import com.google.cloud.storage.BucketInfo;
 import com.google.cloud.storage.Storage;
 import com.google.cloud.storage.StorageOptions;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 @Service
 public class ImageService {
@@ -17,6 +24,8 @@ public class ImageService {
     private ImageRepo imageRepo;
     @Autowired
     private HotelService hotelService;
+    @Value("${GOOGLE_APPLICATION_CREDENTIALS}")
+    private String GOOGLE_APPLICATION_CREDENTIALS;
 
     public void addImage(int hotelId, Image image) {
         if(hotelService.exists(hotelId)){
@@ -32,15 +41,15 @@ public class ImageService {
     }
 
     @Bean
-    public void cloudStorage() throws Exception{
-        //instantiate the client
-        Storage storage = StorageOptions.getDefaultInstance().getService();
+    public void cloudStorage() throws IOException {
 
-        //new bucket name
-        String bucketName = "bonfire-adventures-bucket";
+        Credentials credentials = GoogleCredentials.fromStream(new FileInputStream("src/main/resources/bonfire-adventures-339612-1d713623ba3c.json"));
+
+        //instantiate the client
+        Storage storage = StorageOptions.newBuilder().setCredentials(credentials).build().getService();
 
         //create bucket
-        Bucket bucket = storage.create(BucketInfo.of(bucketName));
+        Bucket bucket = storage.create(BucketInfo.of("bonfire-adventures-safari-bucket"));
 
         System.out.println(bucket.getName());
 
